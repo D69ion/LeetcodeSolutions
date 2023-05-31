70e3e0798d157c66c1de7b47a9193f4e2d7e3d12
D69ion <dmitriy.shtele@gmail.com>
2023-05-31 14:14:52 +0400

    complete

--- LeetcodeSolutions/Algorithms/Easy/13 Roman to Integer.cs
+++ LeetcodeSolutions/Algorithms/Easy/13 Roman to Integer.cs
@@ -6,17 +6,61 @@ using System.Threading.Tasks;
 
 namespace LeetcodeSolutions.Algorithms.Easy
 {
+    //https://leetcode.com/problems/roman-to-integer/description/
     public class Roman
     {
-        int _index = 1;
+        int _originalSize;
+        string IV = "IV"; //4
+        string IX = "IX"; //9
+        string XL = "XL"; //40
+        string XC = "XC"; //90
+        string CD = "CD"; //400
+        string CM = "CM"; //900
         public int RomanToInt(string s)
         {
-            int sum = RomanToDecimal(s[0]);
-
-            for (; _index < s.Length; _index++)
+            _originalSize = s.Length;
+            int sum = 0;
+            s = s.Replace(IV, String.Empty);
+            if (s.Length != _originalSize)
             {
+                sum += (_originalSize - s.Length) / 2 * 4;
+                _originalSize = s.Length;
+            }
+            s = s.Replace(IX, String.Empty);
+            if (s.Length != _originalSize)
+            {
+                sum += (_originalSize - s.Length) / 2 * 9;
+                _originalSize = s.Length;
+            }
+            s= s.Replace(XL, String.Empty);
+            if (s.Length != _originalSize)
+            {
+                sum += (_originalSize - s.Length) / 2 * 40;
+                _originalSize = s.Length;
+            }
+            s=s.Replace(XC, String.Empty);
+            if (s.Length != _originalSize)
+            {
+                sum += (_originalSize - s.Length) / 2 * 90;
+                _originalSize = s.Length;
+            }
+            s = s.Replace(CD, String.Empty);
+            if (s.Length != _originalSize)
+            {
+                sum += (_originalSize - s.Length) / 2 * 400;
+                _originalSize = s.Length;
+            }
+            s = s.Replace(CM, String.Empty);
+            if (s.Length != _originalSize)
+            {
+                sum += (_originalSize - s.Length) / 2 * 900;
+                _originalSize = s.Length;
+            }
 
-                sum += RomanToDecimal(s[_index]);
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                sum += RomanToDecimal(s[i]);
             }
 
             return sum;
